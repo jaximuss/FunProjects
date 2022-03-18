82e6b0596a5676127b12a36b582720f5dbc0c27b
jaximuss <chidiumeo@gmail.com>
2022-03-18 10:57:36 +0100

    a clock with a stop and start button... its a clock not a stopwatch

---
+++ Digital clock/Clock.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace Digital_clock
+{
+    partial class Clock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 49);
+            this.label1.MaximumSize = new System.Drawing.Size(500, 500);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00:00:00";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(211, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(211, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Clock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 147);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Clock";
+            this.Text = "Clock";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Digital clock/Clock.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Digital_clock
+{
+    public partial class Clock : Form
+    {
+        Timer start = new Timer();
+
+        public Clock()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //when this button is clicked start the clock
+
+            start.Interval = 1000; //millisecond which is 1 second
+
+            start.Tick += new EventHandler( this.OnTick );
+
+            //the start the timer
+            start.Start();
+
+        }
+            
+        //timer eveent handler
+        private void OnTick(object sender, EventArgs e)
+        {
+            //lets get the current time of our PC   
+
+            int hours = DateTime.Now.Hour;
+            int minutes = DateTime.Now.Minute;
+            int seconds = DateTime.Now.Second;
+
+            //time that will be displayed on the screen
+            string displayedTime = "";
+
+            //formatting
+            if (hours < 10)
+            {
+                displayedTime += "0" + hours;
+            }
+            else
+            {
+                displayedTime += hours;
+            }
+
+            displayedTime += ":";
+
+            if (minutes < 10)
+            {
+                displayedTime += "0" + minutes;
+            }
+            else
+            {
+                displayedTime += minutes;
+            }
+
+            displayedTime += ":";
+
+            if (seconds < 10)
+            {
+                displayedTime += "0" + seconds;
+            }
+            else
+            {
+                displayedTime += seconds;
+            }
+
+
+            label1.Text = displayedTime;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            start.Stop();
+        }
+    }
+}
